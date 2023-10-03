39b4470cd43b5a63f4ddd7e8a007f05458d96ee8
marty1544 <marty1544@Ubuntu-22-280923-2.myguest.virtualbox.org>
2023-10-03 17:03:24 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "http://www.example.com",
+    "description": """
+        Real estate module to show available properties.
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
---
+++ models/property.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    # This is a table name in DB wil be set to estate_property
+    _name = 'estate.property'
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    best_offer = fields.Float(string="Best Offer")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area(sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation", default='north')
