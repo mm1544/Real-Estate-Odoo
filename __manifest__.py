d8dd38c2e1978d00d3eed8928e96698a49443aad
Martynas Minskis <martynas.minskis@gmail.com>
2023-12-28 18:01:08 +0000

    Add groups

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,10 @@
     "category": "Sales",
     "depends": ["base"],
     "data": [
+        # Groups
         "security/ir.model.access.csv",
+        "security/res_groups.xml",
+
         "views/property_view.xml",
         "views/property_type_view.xml",
         "views/property_tag_view.xml",
